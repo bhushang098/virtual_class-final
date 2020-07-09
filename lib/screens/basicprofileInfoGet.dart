630ca49d27b81b4cfb76378f31be3091047451b1
Administrator <gurnulebhushan2091999@gmail.com>
2020-07-10 04:19:37 +0530

    created new File For Tab layout

---
+++ lib/screens/basicprofileInfoGet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GetBasicProfileInfo extends StatefulWidget {
+  @override
+  _GetBasicProfileInfoState createState() => _GetBasicProfileInfoState();
+}
+
+class _GetBasicProfileInfoState extends State<GetBasicProfileInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
